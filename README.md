ad340fe97f83b4bf6c24457a6727600ab0c87c34
danny <dtylman@gmail.com>
2025-08-09 20:01:41 +0300

    first poc

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # saatool
 Supervised Aided Translation Tool 
+
+```
+export ANDROID_HOME=$HOME/Android/Sdk
+export ANDROID_NDK_HOME=$HOME/Android/Sdk/ndk/29.0.13846066
+export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
+fyne package -os android -appID com.saatool.app
+```
---
+++ package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ANDROID_HOME=$HOME/Android/Sdk
+export ANDROID_NDK_HOME=$HOME/Android/Sdk/ndk/29.0.13846066
+export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
+SOURCE_DIR=$(pwd)/cmd/saatool
+
+# ~/go/bin/fyne package --target android/arm64 --app-id org.saatool.app --source-dir $SOURCE_DIR --icon $SOURCE_DIR/icon.png --name "SAATool" 
+~/go/bin/fyne package --target android/amd64 --app-id org.saatool.app --source-dir $SOURCE_DIR --icon $SOURCE_DIR/icon.png --name "SAAToolEmu" 
