cb53b27d4bdaae63409c2aa74803c3c3258df06b
Danny Tylman <danny.tylman@jifiti.com>
2025-08-13 16:02:08 +0300

    dev

--- ui/translationview.go
+++ ui/translationview.go
@@ -9,23 +9,34 @@ import (
 )
 
 type TranslationView struct {
-	OnClose func()
-	View    fyne.CanvasObject
-	project *translation.Project
-	txt     *widgets.BidiText
+	OnClose   func()
+	View      fyne.CanvasObject
+	project   *translation.Project
+	txt       *widgets.BidiText
+	source    bool // true for source language, false for target language
+	paragraph int  // current paragraph index
+	offset    int  // current offset in the paragraph (this the word index)
 }
 
 func NewTranslationView(project *translation.Project) *TranslationView {
 	tv := &TranslationView{
-		project: project,
+		project:   project,
+		source:    true, // default to source language
+		paragraph: 0,    // start with the first paragraph
+		offset:    0,    // start at the beginning of the paragraph
 	}
-	backBtn := widget.NewButton("Close", tv.onClose)
+
+	toolBar := container.NewHBox(
+		widget.NewLabel("Translation View"),
+		widget.NewSeparator(),
+		widget.NewButton("Back", tv.onClose),
+	)
 
 	txt := widgets.NewBidiText(project.Target.Paragraphs[0].Text)
 
 	content := container.NewBorder(
 		nil,
-		backBtn,
+		toolBar,
 		nil,
 		nil,
 		txt,
